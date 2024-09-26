093d7e1b21a9cc08d1ae181436aa6fc405fffbef
GiovanniBarros <GiovanniBarros@GCI-WWC-LTGBAR>
2024-09-26 16:07:17 -0600

    Add project files.

---
+++ fakeRepo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fakeRepo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static CalculateCosts ( int base, int disc)
+        {
+            Console.WriteLine("Calculating costs...");
+        }
+    }
+}
